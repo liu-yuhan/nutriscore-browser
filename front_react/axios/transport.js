89de5b4f00d4353d543f58cd3a74eca881528dcf
liu-yuhan <yuhan.liu2013@gmail.com>
2019-05-22 10:02:08 +0200

    add axios set function transport to pass argument

---
+++ front_react/axios/transport.js
@@ -0,0 +1,18 @@
+//method for comunication between front and back
+
+import axios from "axios"
+
+function transport(url,data={},method="GET"  ){
+    if(method==="GET"){
+        let paramStr = ''
+        Object.keys(data).forEach(key=>{
+            paramStr += key +"=".data[key]+"&"+data[key]
+        })
+        if(paramStr){
+            paramStr.substr(0,paramStr.length-1)
+        }
+        return axios.get(url+'?'+paramStr)
+    }else{
+        return axios.post(url,data)
+    }
+}
