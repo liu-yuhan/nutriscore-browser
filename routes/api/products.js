935cf18c962b780ac2b79615a408c70ac1ec87cb
alexg <alexandre.gracias@coding-academy.fr>
2019-05-22 15:56:45 +0200

    produtcs

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/Users');
 const Product = require('../../models/Products');
 const auth = require('../middleware/auth');
