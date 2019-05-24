a5997a95b4a5cc54c795239776eeae4fdb364693
Ronan <ronan.perrier37@gmail.com>
2019-05-24 17:33:27 +0200

    Add some results on result page

--- front_react/src/Components/Result.js
+++ front_react/src/Components/Result.js
@@ -1,50 +1,47 @@
-import React, { Component } from 'react'
-import Axios from 'axios';
+import React, { Component } from "react";
+import Axios from "axios";
 
+class Result extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idScan: ""
+    };
+  }
 
-class Result extends Component{
-    
-    constructor(props){
-        super(props)
-        this.state={
-            idScan:'',
-            resultScan:{},
-        }
-    }
+  componentDidMount() {
+    Axios.get(
+      "https://fr.openfoodfacts.org/api/V0/produit/" +
+        this.props.match.params.id +
+        ".json"
+    ).then(result => {
+      this.setState({ resultScan: result.data });
 
-    componentDidMount(){
-        Axios.get('https://fr.openfoodfacts.org/api/V0/produit/'+this.props.match.params.id +'.json')
-        .then(result=>{
-            this.setState({resultScan:result.data})
+      console.log(this.state.resultScan.product.product_name);
+    });
+  }
 
-            console.log(this.state.resultScan.product.generic_name_fr)
-        })
-    }
-
-    render(){
-
-        const loadCont=this.state.resultScan
-
-        const loadFinish=(
-            <div>
-                Hello, you're on page {this.props.match.params.id}
-                <br/>
-                Le produit est du : {this.state.resultScan.product.generic_name_fr}
-            </div>
-        )
-
-        const loading=(
-            <div>
-                Chargement des données en cours
-            </div>
-        )
-
-        return(
-            <div>
-                {loadCont===[] ? loading : loadFinish}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {!this.state.resultScan ? (
+          <div>Chargement des données en cours</div>
+        ) : (
+          <div className='jumbotron mx-auto mt-2'>
+            Hello, you're on page {this.props.match.params.id}
+            <br />
+            Le produit est du : {this.state.resultScan.product.product_name}
+            <br />
+            Origine : {this.state.resultScan.product.origins==''?'Non définie': this.state.resultScan.product.origins}
+            <br />
+            <img className='text-right' src={this.state.resultScan.product.image_front_url}/>
+            <br/>
+            Huile de Palme : {this.state.resultScan.product.ingredients_from_palm_oil_n=='1' ? 'Oui' : 'Non' }
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Result;
