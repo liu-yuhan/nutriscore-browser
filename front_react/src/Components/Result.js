f195a17666b5b70c12ae4d28320ddb1b9a1cfc2c
Ronan <ronan.perrier37@gmail.com>
2019-05-24 19:58:12 +0200

    Add another result to Result.js => Packaging

--- front_react/src/Components/Result.js
+++ front_react/src/Components/Result.js
@@ -14,11 +14,15 @@ class Result extends Component {
       "https://fr.openfoodfacts.org/api/V0/produit/" +
         this.props.match.params.id +
         ".json"
-    ).then(result => {
-      this.setState({ resultScan: result.data });
+    )
+      .then(result => {
+        this.setState({ resultScan: result.data });
 
-      console.log(this.state.resultScan.product.product_name);
-    });
+        console.log(this.state.resultScan.product.product_name);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
@@ -26,17 +30,32 @@ class Result extends Component {
       <div>
         {!this.state.resultScan ? (
           <div>Chargement des données en cours</div>
+        ) : !this.state.resultScan.product.product_name ? (
+          <div className="jumbotron mx-auto mt-2">
+            <p>Produit non trouvé</p>
+          </div>
         ) : (
-          <div className='jumbotron mx-auto mt-2'>
+          <div className="jumbotron mx-auto mt-2">
             Hello, you're on page {this.props.match.params.id}
             <br />
             Le produit est du : {this.state.resultScan.product.product_name}
             <br />
-            Origine : {this.state.resultScan.product.origins==''?'Non définie': this.state.resultScan.product.origins}
+            Origine :{" "}
+            {this.state.resultScan.product.origins == ""
+              ? "Non définie"
+              : this.state.resultScan.product.origins}
             <br />
-            <img className='text-right' src={this.state.resultScan.product.image_front_url}/>
-            <br/>
-            Huile de Palme : {this.state.resultScan.product.ingredients_from_palm_oil_n=='1' ? 'Oui' : 'Non' }
+            <img
+              className="text-right"
+              src={this.state.resultScan.product.image_front_url}
+            />
+            <br />
+            Huile de Palme :{" "}
+            {this.state.resultScan.product.ingredients_from_palm_oil_n == "1"
+              ? "Oui"
+              : "Non"}
+            <br />
+            Packaging : {this.state.resultScan.product.packaging}
           </div>
         )}
       </div>
