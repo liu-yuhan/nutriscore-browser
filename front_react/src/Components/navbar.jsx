9633c3e7a840e4cba9259dee16461e65409340a5
alexg <alexandre.gracias@coding-academy.fr>
2019-05-29 10:26:57 +0200

    navbar MAJ +  start donuts + CSS

---
+++ front_react/src/Components/navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./style.css";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg fixed-top">
+        <img className="logo2" src="final.gif" />
+      </nav>
+    );
+  }
+}
