db15506c780920eb8d73232f08058b7bc96ebcbc
titi <thibaud.levy@coding-academy.fr>
2019-05-29 09:47:39 +0200

    History and ProductList components

---
+++ front_react/src/Components/History.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+export default class History extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+const styles 
