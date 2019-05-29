7e1124809ec0d59d5871a5e13134c828da79f412
liu-yuhan <yuhan.liu2013@gmail.com>
2019-05-29 13:52:46 +0200

    cover page is finished, redirect to profile page is token is valid, otherwise to login

--- front_react/src/Container/Cover/cover.jsx
+++ front_react/src/Container/Cover/cover.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 import Cover_img from "../../Components/cover";
-
+import jwt_decode from "jwt-decode";
 export default class Cover extends Component {
   constructor(props) {
     super(props);
@@ -14,11 +14,33 @@ export default class Cover extends Component {
     document.body.style.background = "#07a995";
   }
 
+  enterAppHandler = event => {
+    console.log("button clicked");
+    const getToken = localStorage.getItem("jwToken");
+    if (getToken) {
+      console.log("gotToken");
+      const decodeToken = jwt_decode(getToken);
+      const currentTime = Date.now() / 1000;
+      if (decodeToken.exp >= currentTime) {
+        this.props.history.replace("/profile");
+      }
+    } else {
+      console.log("notoken");
+      this.props.history.replace("/login");
+    }
+  };
+
   render() {
     return (
       <div id="coverPage" className="h-100 ">
         <Cover_img />
-        <Button className="fixed-bottom" variant="secondary" size="lg" block>
+        <Button
+          className="fixed-bottom"
+          variant="secondary"
+          size="lg"
+          block
+          onClick={this.enterAppHandler.bind(this)}
+        >
           ENTER
         </Button>
       </div>
