1b6a22ab8a5e6ebc6b4e9d59eb3b3ba26ab5d778
Ronan <ronan.perrier37@gmail.com>
2019-05-31 13:39:03 +0200

    Repairing Back

--- routes/api/products.js
+++ routes/api/products.js
@@ -30,11 +30,11 @@ router.post("/:id", auth, (req, res) => {
           { new: true }
         )
           .then(result => res.json(product))
-          .catch(err => console.log("patate", err));
+          .catch(err => console.log(err));
       }
       console.log(req.user.id);
     })
-    .catch(error => console.log("carrotte", error));
+    .catch(error => console.log(error));
 });
 
 router.get("/:user_id", async (req, res) => {
@@ -45,77 +45,8 @@ router.get("/:user_id", async (req, res) => {
       res.json(user_products);
     }
   } catch (error) {
-    res.status(500).json({ msg: "nique ta mère" });
+    res.status(500).json({ msg: "Internal server error" });
   }
 });
 
-<<<<<<< HEAD
-=======
-router.post('/:id', auth, async (req, res) => {
-  try {
-    const newProduct = new Product({
-      barcode: req.params.id,
-      user: req.user.id
-    });
-    const product = await newProduct.save();
-
-    res.json(product);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send('Server Error');
-  }
-})
-
-router.post('/comments/:id',
-[
-   auth,
-   [
-     check('text', 'Need a comment !')
-     .not()
-     .isEmpty()
-   ]
-],
-
-  async(req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-      res.status(400).json({error: errors})
-    };
-    try{
-    let user = await User.findOne({ email: email })
-    let product = await Products.findById(req.params.id);
-
-    const newComment = {
-      text: req.body.text, 
-      user: req.user.id
-    };
-
-    product.comments.unshift(newComment);
-    await product.save()
-    res.json({product, user});
-  } catch(err){
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  };
-  }
-);
-
-router.delete('/comments/delete/:id', auth, async (req, res) => {
-  try{
-  const comment = await Product.findById(req.params.id);
-
-    if(comment.user.toString() !== req.user.id){
-      return res.status(401).json({msg: 'Not authorize to delete this comment'})
-    } 
-    await comment.remove();
-    res.json({msg: 'Comment deleted !'})
-
-  } catch(err){
-    res.status(400).json({err: error})
-  }
-})
-
-
-
->>>>>>> yuhan
 module.exports = router;
