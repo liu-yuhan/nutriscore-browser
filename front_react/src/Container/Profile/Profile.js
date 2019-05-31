f59bd2a124212cff7d8722dd3318ab54f571a91d
thibaud <levythibaud@me.com>
2019-05-31 12:27:16 +0200

    suppression bug dans constructeur profile

--- front_react/src/Container/Profile/Profile.js
+++ front_react/src/Container/Profile/Profile.js
@@ -1,52 +1,39 @@
-import React, { Component } from "react";
-import { Card } from "react-bootstrap";
-import Header from "../../Components/header";
-import "../container_style.css";
-import { connect } from "react-redux";
-import { profile } from "../../redux/action";
-import jwt_decode from "jwt-decode";
-import axios from "axios";
-import moment from "moment";
-import MenuProfile from "../../Components/MenuProfile";
+import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import Header from '../../Components/header';
+import '../container_style.css';
+import { connect } from 'react-redux';
+import { profile } from '../../redux/action';
+import jwt_decode from 'jwt-decode';
+import axios from 'axios';
+import moment from 'moment';
+import MenuProfile from '../../Components/MenuProfile';
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: "",
-      name: "",
-      email: "",
-      date: "",
-      token: ""
+      id: '',
+      name: '',
+      email: '',
+      date: '',
+      token: ''
     };
-    const getToken = localStorage.getItem("jwToken");
-
-    if (!getToken) {
-      this.props.history.push("/login");
-    } else {
-      const decodeToken = jwt_decode(getToken);
-      console.log(decodeToken.user.id);
-      const userId = decodeToken.user.id;
-      this.state.id = userId;
-      this.state.token = getToken;
-      console.log("State.id : ", this.state);
-      this.props.profile(this.state);
-    }
   }
 
   componentDidMount() {
-    const getToken = localStorage.getItem("jwToken");
+    const getToken = localStorage.getItem('jwToken');
 
     if (!getToken) {
-      this.props.history.push("/login");
+      this.props.history.push('/login');
     } else {
       const decodeToken = jwt_decode(getToken);
       this.setState({ id: decodeToken.user.id });
       axios
-        .get("http://localhost:5000/api/profile", {
+        .get('http://localhost:5000/api/profile', {
           headers: {
-            "Content-Type": "application/json",
-            "x-auth-token": getToken
+            'Content-Type': 'application/json',
+            'x-auth-token': getToken
           }
         })
         .then(response => {
@@ -72,15 +59,15 @@ class Profile extends Component {
     return (
       <div>
         <Header />
-        <Card border="light">
+        <Card border='light'>
           <MenuProfile props={this.props} />
-          <Card.Img variant="top" src="https://picsum.photos/300" />
+          <Card.Img variant='top' src='https://picsum.photos/300' />
           <Card.Header>User's Profile</Card.Header>
           <Card.Body>
             <Card.Title>Username : {this.state.name}</Card.Title>
             <Card.Text>Email : {this.state.email}</Card.Text>
             <Card.Text>
-              Created at : {moment(this.state.date).format("MMMM Do YYYY")}
+              Created at : {moment(this.state.date).format('MMMM Do YYYY')}
             </Card.Text>
           </Card.Body>
         </Card>
