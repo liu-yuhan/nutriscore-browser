d5e47519cbee6c8a9ccbe1e861e25e81b9518b49
yuhan <yuhan.liu2013@gmail.com>
2019-06-05 12:24:17 +0200

    responsive all work, except width 767 & 768 wiredgit add --all!

--- front_react/src/Components/header.jsx
+++ front_react/src/Components/header.jsx
@@ -1,15 +1,13 @@
-import React, {Component} from 'react';
-import "./style.css"
+import React, { Component } from "react";
+import "./res_style.css";
 
-    
 class Header extends Component {
-
-render() {
-    return(
-        <div className='header mx-auto py-auto' >
-            <h1 className='logo text-center' > UNCOVER </h1>
-        </div>)
+  render() {
+    return (
+      <div className="header mx-auto py-auto">
+        <h1 className="logo text-center"> UNCOVER </h1>
+      </div>
+    );
+  }
 }
-
-}
-export default Header
+export default Header;
